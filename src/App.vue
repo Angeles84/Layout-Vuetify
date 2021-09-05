604740278201f030477b567e7b9bb04d9fafed17
Angeles <angie.oliva@gmail.com>
2021-09-05 19:52:21 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+     <v-app-bar
+      app
+      color="accent-4"
+      light
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Mi app</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn plain>
+        INICIO
+      </v-btn>
+
+      <v-btn plain>
+        BUSCAR
+      </v-btn>
+
+      <v-btn plain>
+        ACERCA DE
+      </v-btn>
+     
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="mt-5">
+    <v-row>
+      <v-col cols="12" md="6" lg="4" v-for="(personaje, index) in personajes" :key="index">
+        <v-card
+          max-width="344"
+        >
+          <v-img
+            :src="personaje.imagen"
+            height="340px"
+          >
+          </v-img>
+
+          <v-card-title>
+            {{personaje.nombre}}
+          </v-card-title>
+       </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      personajes: [
+        {
+          'id' : 1 ,
+          'nombre' : 'Morty' ,
+          'imagen' : 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+        },
+        {
+          'id' : 2 ,
+          'nombre' : 'Rick' ,
+          'imagen' : 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+        },
+        {
+          'id' : 3 ,
+          'nombre' : 'Summer' ,
+          'imagen' : 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+        },
+        {
+          'id' : 4 ,
+          'nombre' : 'Beth' ,
+          'imagen' : 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+        },
+        {
+          'id' : 5 ,
+          'nombre' : 'Jerry' ,
+          'imagen' : 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+        }
+      ]
+    }),
   }
-}
 </script>
