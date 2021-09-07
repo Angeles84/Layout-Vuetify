e1f7a4dccc0315c86398fd2cacb120e80b818cfd
Angeles <angie.oliva@gmail.com>
2021-09-06 21:53:00 -0300

    tercer commit

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@
       app
       color="accent-4"
       light
+      fixed
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Mi app</v-toolbar-title>
 
@@ -25,9 +26,53 @@
      
     </v-app-bar>
 
+    <v-navigation-drawer
+      app
+      absolute
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+      width="20%"
+      v-model="drawer" 
+      temporary
+    >
+      <v-list>
+        <v-list-item
+          v-for="([icon, text], i) in items"
+          :key="i"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
       <router-view/>
     </v-main>
+
+    <v-footer
+    color="primary lighten-1"
+    padless
+    class="mt-8"
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
   </v-app>
 </template>
 
@@ -37,7 +82,13 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
-  }),
+    items: [
+      ['mdi-email', 'Inbox'],
+      ['mdi-account-supervisor-circle', 'Supervisors'],
+      ['mdi-clock-start', 'Clock-in'],
+    ],
+    drawer: false,
+    
+    }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <v-container class="mt-5">
     <v-row>
-      <v-col cols="12" sm="6" lg="4" v-for="(personaje, index) in personajes" :key="index">
+      <v-col cols="12" sm="6" lg="4" v-for="personaje in personajes" :key="personaje.id">
         <v-card
-        
+          color="blue darken-3"
+          dark
         >
           <v-img
             :src="personaje.imagen"
